03eec2a74ee5006d90e589068663ba270bcd90d0
shogunskinoski <whitebirdsdk@gmail.com>
2023-04-07 16:20:44 +0300

    email validation extension

--- lib/core/extension/string_extension.dart
+++ lib/core/extension/string_extension.dart
@@ -1,5 +1,13 @@
+import 'package:cread/core/constants/app/app_constants.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 extension StringLocalization on String {
   String get locale => this.tr();
+}
+
+extension StringExtension on String {
+  bool isValidEmail() {
+    final RegExp regex = RegExp(AppConstants.EMAIL_REGEX);
+    return regex.hasMatch(this);
+  }
 }
