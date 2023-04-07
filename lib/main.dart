807223dd52a3ace04f9ab849b118285f42ac13b3
shogunskinoski <whitebirdsdk@gmail.com>
2023-04-07 16:19:52 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,15 @@ import 'package:easy_localization/easy_localization.dart';
 import 'core/init/navigation/navigation.dart';
 import 'core/init/navigation/navigation_route.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
