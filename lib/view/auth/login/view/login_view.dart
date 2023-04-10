13350595f0a6c183f4c58b020e97d966bf796747
shogunskinoski <whitebirdsdk@gmail.com>
2023-04-10 20:17:03 +0300

    refactoring auth views

--- lib/view/auth/login/view/login_view.dart
+++ lib/view/auth/login/view/login_view.dart
@@ -2,12 +2,14 @@
 
 import 'package:cread/view/auth/login/service/login_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:cread/view/auth/login/viewmodel/login_view_model.dart';
 import 'package:cread/core/base/view/base_view.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 import '../../../../core/init/firebase/auth_repository.dart';
+import '../../../../feature/widget/TextField/custom_textfield.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -32,39 +34,53 @@ class _LoginViewState extends State<LoginView> {
 
   Widget buildScaffold() {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
       body: buildBody(),
     );
   }
 
   Widget buildBody() {
-    return Column(
-      children: [
-        Expanded(child: buildForm()),
-        buildLoginButton(),
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 197, 225, 248),
+      ),
+      child: Column(
+        children: [
+          Expanded(child: Container()),
+          Expanded(flex: 2, child: buildForm()),
+        ],
+      ),
     );
   }
 
   Form buildForm() {
     return Form(
-      child: Column(
-        children: [
-          Expanded(child: Container()),
-          Expanded(
-              flex: 2,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(child: Container()),
+            Expanded(
+              flex: 4,
               child: SingleChildScrollView(
                 child: Column(
                   children: [
                     buildEmailField(),
                     buildPasswordField(),
+                    buildLoginButton(),
                     buildRegisterButton(),
                   ],
                 ),
-              )),
-        ],
+              ),
+            ),
+            //do account text as blue color
+          ],
+        ),
       ),
     );
   }
@@ -74,17 +90,9 @@ class _LoginViewState extends State<LoginView> {
       margin: const EdgeInsets.symmetric(vertical: 10),
       width: MediaQuery.of(context).size.width * 0.8,
       child: Observer(builder: (_) {
-        return TextFormField(
-          onChanged: (value) {
-            viewModel.changeEmail(value);
-          },
-          decoration: InputDecoration(
-            errorText: viewModel.emailError(),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            labelText: 'Email',
-          ),
+        return EmailTextField(
+          onChanged: viewModel.changeEmail,
+          emailError: viewModel.emailError,
         );
       }),
     );
@@ -95,44 +103,61 @@ class _LoginViewState extends State<LoginView> {
       margin: const EdgeInsets.symmetric(vertical: 10),
       width: MediaQuery.of(context).size.width * 0.8,
       child: Observer(builder: (_) {
-        return TextFormField(
-          obscureText: viewModel.isPasswordVisible,
-          onChanged: (value) {
-            viewModel.setPassword(value);
-          },
-          decoration: InputDecoration(
-            suffixIcon: IconButton(
-              icon: Icon(viewModel.isPasswordVisible
-                  ? Icons.visibility
-                  : Icons.visibility_off),
-              onPressed: () {
-                viewModel.changePasswordVisibility();
-              },
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            ),
-            labelText: 'Password',
-          ),
-        );
+        return PasswordTextField(
+            onChanged: viewModel.setPassword,
+            onPressed: viewModel.changePasswordVisibility,
+            obscureText: viewModel.isPasswordVisible);
       }),
     );
   }
 
   Observer buildLoginButton() {
     return Observer(builder: (_) {
-      return ElevatedButton(
-        onPressed: () {
-          viewModel.login();
-        },
-        child: Text('Login'),
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+            alignment: Alignment.center,
+            elevation: MaterialStateProperty.all(10),
+          ),
+          onPressed: () {
+            viewModel.login();
+          },
+          child: Text(
+            'Login',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
       );
     });
   }
 
-  ElevatedButton buildRegisterButton() {
-    return ElevatedButton(
-        onPressed: () => viewModel.navigateToRegisterPage(),
-        child: const Text('Register'));
+  Padding buildRegisterButton() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: RichText(
+          text: TextSpan(children: [
+        TextSpan(
+          text: 'Don\'t have an account? ',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+        TextSpan(
+            text: 'Register',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 18,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                viewModel.navigateToRegisterPage();
+              }),
+      ])),
+    );
   }
 }
--- lib/view/auth/splash/view/splash.dart
+++ lib/view/auth/splash/view/splash.dart
@@ -28,7 +28,7 @@ class _SplashViewState extends State<SplashView> {
     );
   }
 
-  Widget buildScaffold(BuildContext context, SplashViewModel viewModel) {
+  Scaffold buildScaffold(BuildContext context, SplashViewModel viewModel) {
     PageController _pageController = PageController(initialPage: 0);
     viewModel.pageController = _pageController;
     return Scaffold(
@@ -49,20 +49,18 @@ class _SplashViewState extends State<SplashView> {
     );
   }
 
-  Widget pageBuilder(SplashViewModel viewModel, int index) {
+  Column pageBuilder(SplashViewModel viewModel, int index) {
     return Column(
       children: [
         Expanded(
-          flex: 10,
+          flex: 4,
           child: Lottie.asset(viewModel.splashItems[index].splashPath),
         ),
         Expanded(
           flex: 2,
           child: splashContent(viewModel, index),
         ),
-        Spacer(flex: 1),
         Expanded(flex: 1, child: indicatorBuilder(viewModel)),
-        Spacer(flex: 1),
         Expanded(
           flex: 1,
           child: nextPageButton(viewModel),
@@ -73,32 +71,60 @@ class _SplashViewState extends State<SplashView> {
 
   Column splashContent(SplashViewModel viewModel, int index) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(viewModel.splashItems[index].title),
-        Text(viewModel.splashItems[index].description),
+        Align(
+          alignment: Alignment.center,
+          child: Text(viewModel.splashItems[index].title,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold)),
+        ),
+        Container(
+          width: double.infinity,
+          child: Text(
+            viewModel.splashItems[index].description,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+        ),
       ],
     );
   }
 
-  TextButton nextPageButton(SplashViewModel viewModel) {
+  Padding nextPageButton(SplashViewModel viewModel) {
     // create a extension for this
-    return TextButton(
-      style: ButtonStyle(
-        fixedSize: MaterialStateProperty.all(Size(150, 50)),
-        backgroundColor: MaterialStateProperty.all(Colors.blue),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: double.infinity,
+        child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              Colors.blue,
+            ),
+          ),
+          onPressed: () {
+            viewModel.nextPage();
+          },
+          child: Observer(builder: (_) {
+            return Text(
+              viewModel.currentPage == viewModel.splashItems.length - 1
+                  // add localization of this texts on translation file
+                  ? 'Get Started'
+                  : 'Next',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            );
+          }),
+        ),
       ),
-      onPressed: () {
-        viewModel.nextPage();
-      },
-      child: Observer(builder: (_) {
-        return Text(
-          viewModel.currentPage == viewModel.splashItems.length - 1
-              // add localization of this texts on translation file
-              ? 'Get Started'
-              : 'Next',
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        );
-      }),
     );
   }
 
