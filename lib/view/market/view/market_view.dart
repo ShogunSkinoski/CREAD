18b607ae5a494b563c4d2aac0cad5b0a3627b975
shogunskinoski <whitebirdsdk@gmail.com>
2023-04-14 01:03:50 +0300

    market page

---
+++ lib/view/market/view/market_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MarketView extends StatefulWidget {
+  const MarketView({super.key});
+
+  @override
+  State<MarketView> createState() => _MarketViewState();
+}
+
+class _MarketViewState extends State<MarketView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
